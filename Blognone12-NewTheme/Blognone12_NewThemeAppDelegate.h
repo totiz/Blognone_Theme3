7700ab9e5ed2654d44684ab85894d702bcafef06
Nattapon Nimakul <totizy@gmail.com>
2011-03-23 17:03:18 +0700

    Initial Commit

---
+++ Blognone12-NewTheme/Blognone12_NewThemeAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  Blognone12_NewThemeAppDelegate.h
+//  Blognone12-NewTheme
+//
+//  Created by Nattapon on 3/23/11.
+//  Copyright 2011 iphone developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Blognone12_NewThemeAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
